7dfd3bfae62b28b976ed9f6abc41d6b551005e30
Belli-Isa <Isabellicaetanopereira2008@gmail.com>
2024-11-15 11:08:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Movies',
+    'Em qual filme aparece a frase: "Que a Força esteja com você"?',
+    'O filme é Star Wars'
+)
+
+criaCartao(
+    'Movies',
+    'Qual é o nome do vilão principal em Os Vingadores (2012)?',
+    'O vilão é Loki'
+)
+
+criaCartao(
+    'Movies',
+    'Quem é a heroína principal em Valente?',
+    'A heroína é Merida'
+)
