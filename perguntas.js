7718706d7baa0ea409711acb9134dda38b993876
Belli-Isa <Isabellicaetanopereira2008@gmail.com>
2024-11-21 14:14:38 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,89 @@
 criaCartao(
     'Movies',
-    'Em qual filme aparece a frase: "Que a Força esteja com você"?',
+    'Em qual filme aparece a seguinte frase: "Que a Força esteja com você"?',
     'O filme é Star Wars'
 )
 
 criaCartao(
     'Movies',
     'Qual é o nome do vilão principal em Os Vingadores (2012)?',
-    'O vilão é Loki'
+    'O vilão em Vingadores é Loki'
 )
 
 criaCartao(
     'Movies',
-    'Quem é a heroína principal em Valente?',
-    'A heroína é Merida'
+    'Em Homem de Ferro, como se chama a inteligência artificial que ajuda Tony Stark?',
+    'A inteligência artificial é J.A.R.V.I.S'
+)
+
+criaCartao(
+    'Movies',
+    'Qual o heroi é conhecido como "Cavaleiro das Trevas"?',
+    'O heroi é o Batman'
+)
+
+criaCartao(
+    'Movies',
+    'Em It: A Coisa, qual o nome do palhaço?',
+    'O nome do palhaço é Pennywise'
+)
+
+criaCartao(
+    'Serie',
+    'Em Peaky Blinder, qual é o nome do líder da familia Shelby?',
+    'O líder é Thomas Shelby'
+)
+
+criaCartao(
+    'Serie',
+    'Em Euphoria, qual é o nome da personagem principal interpretada por Zendaya?',
+    'O nome da personagem principal é Rue Bennet'
+)
+
+criaCartao(
+    'Serie',
+    'Em House of the Dragon, qual é o nome da casa que disputa o Trono de Ferro?',
+    'A casa que disputa o trono é a Casa Targaryen'
+)
+
+criaCartao(
+    'Serie',
+    'Em Arrow, qual o nome da cidade natal de Oliver Queen?',
+    'O nome da cidade é Star City'
+)
+
+criaCartao(
+    'Movies',
+    'Em Homem-Aranha: No Aranhaverso, qual o nome do protagonista principal?',
+    'O protagonista chama-se Miles Morales'
+)
+
+criaCartao(
+    'Movies',
+    'Qual vilão utiliza uma máscara de hockey nos filmes Sexta-Feira 13?',
+    'O vilão que usa essa máscara é Jason Voorhees'
+)
+
+criaCartao(
+    'Movies',
+    'Em Hora do Pesadelo, qual o nome do assassino que ataca as vítimas em seus sonhos?',
+    'O assassino se chama Freddy Krueger'
+)
+
+criaCartao(
+    'Movies',
+    'Qual nome do vilão que rouba a Lua em Meu Malvado Favorito?',
+    'O nome do vilão é Gru'
+)
+
+criaCartao(
+    'Serie',
+    'Em Stranger Things, qual é o nome da dimensão alternativa que os personagens vistam?',
+    'A dimensão se chama Mundo Invertido (Upside Down)'
+)
+
+criaCartao(
+    'Serie',
+    'Na série Arrow, qual é o codinome da irmã de Oliver Queen?',
+    'O codinome dela é Speedy'
 )
